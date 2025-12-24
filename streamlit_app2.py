d90529552b55e0185c876c70a32099389e18eac4
Lipeee20 <dayphilip5@gmail.com>
2025-12-24 10:20:23 +0700

    Update streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -114,10 +114,6 @@ if menu == "Dashboard":
     col2.metric("Nilai Min", df["Nilai"].min())
     col3.metric("Rata-rata", round(df["Nilai"].mean(), 2))
 
-   st.bar_chart(
-    df.set_index("Nama")["Nilai"]
-)
-
     st.bar_chart(df.set_index("Nama")["Nilai"])
 
 
